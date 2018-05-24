c59acdabfe04492485c9d6b918f78c45c5c4343d
Kevin Liozon <kevinliozonpro@gmail.com>
2018-05-24 22:24:16 +0100

    Service for http response from CS is refactored

--- src/app/providers/codeschool/codeschool.service.ts
+++ src/app/providers/codeschool/codeschool.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeschoolService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /**
+   * No need to invoke map() to parse out the response. Method has been removed,
+   * and we simply return the Observable. Default responseType is JSON,
+   * so the response data is already parsed for us
+   *
+   * https://www.codeschool.com/users/kevinliozon.json
+   *
+   * @param {string} id
+   * @returns {Observable<any>}
+   */
+  public getCodeschoolProfile(): Observable<any> {
+    return this.http.get<any>('app/shared/data/mock-codeschool.json');
+  }
+
 }
