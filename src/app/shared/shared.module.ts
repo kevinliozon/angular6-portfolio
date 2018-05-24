bed482ea64fcc8b6bc375d5ed17eb708baaba1f2
Kevin Liozon <kevin@surelygroup.com>
2018-05-24 12:56:07 +0100

    Created pipes and module, pipes are DONE

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PipesModule
   ],
   declarations: []
 })
