4e5a50b04a24f065339b3c72549532630fcaf499
Kevin Liozon <kevin@surelygroup.com>
2018-05-24 17:54:54 +0100

    Added Analytics, Route anim and Card component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipesModule } from './pipes/pipes.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PipesModule
   ],
-  declarations: []
+  declarations: [
+    CardComponent,
+  ],
+  exports: [
+    CardComponent,
+  ]
 })
 export class SharedModule { }
