a13c3b023f201433ef9df8c78afa2e7ca519a90c
Kevin Liozon <kevinliozonpro@gmail.com>
2018-05-24 20:18:12 +0100

    Imported properly cookie banner in shared module. Added missing modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Modules
+import { AppRoutingModule } from '../app-routing.module';
 import { PipesModule } from './pipes/pipes.module';
+import { TranslationsModule } from '../translations/translations.module';
+// Components
 import { CardComponent } from './components/card/card.component';
+import { CookieBannerComponent } from './components/cookie-banner/cookie-banner.component';
 
 @NgModule({
   imports: [
+    AppRoutingModule,
     CommonModule,
-    PipesModule
+    PipesModule,
+    TranslationsModule
   ],
   declarations: [
     CardComponent,
+    CookieBannerComponent,
   ],
   exports: [
     CardComponent,
+    CookieBannerComponent,
   ]
 })
 export class SharedModule { }
